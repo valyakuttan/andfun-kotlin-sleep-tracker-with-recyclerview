2b0bff6108c0f169fc5c5288faa5005f41ddffae
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-26 09:51:43 +0530

    step 03 completed: since recycleview recycles views, view attributes must be resettted to deafault values, otherwise we will encounter bugs

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -39,14 +40,18 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
         holder.textView.text = item.sleepQuality.toString()
 
         // TODO (01) Add an if block and set the sleep quality to red
-        // if sleepQuality is 1 or less.
+        //  if sleepQuality is 1 or less.
 
         // TODO (02) Run the app and see that color does not reset to black.
-
-        // TODO (03) Add an else clause and reset the color if sleepQuality is 2 or higher.
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            // TODO (03) Add an else clause and reset the color if sleepQuality is 2 or higher.
+            holder.textView.setTextColor(Color.BLACK)
+        }
 
         // TODO (04) Run the app again and verify that the color displays correctly
-        // for all sleepQuality values.
+        //  for all sleepQuality values.
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
@@ -57,4 +62,5 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
         return TextItemViewHolder(view)
     }
+
 }

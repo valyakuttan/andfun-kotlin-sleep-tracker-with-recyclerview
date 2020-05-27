a3daac69bd48d6bd27a199094c4c09409f2fe7dd
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-27 11:30:41 +0530

    step 12: add navigation to details fragment when clicked on an image, completed

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -100,13 +100,20 @@ class SleepTrackerFragment : Fragment() {
         })
 
         // TODO (05) Add an observer for navigateToSleepDataQuality.
+        sleepTrackerViewModel.navigateToSleepDataQuality.observe(this, Observer {night ->
+            night?.let {
+                this.findNavController().navigate(SleepTrackerFragmentDirections
+                        .actionSleepTrackerFragmentToSleepDetailFragment(night))
+                sleepTrackerViewModel.onSleepDataQualityNavigated()
+            }
+        })
 
         val manager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = manager
 
         // TODO (02) Replace the Toast message with code to pass nightId to the view model.
-        val adapter = SleepNightAdapter(SleepNightListener { nightId ->
-            Toast.makeText(context, "${nightId}", Toast.LENGTH_LONG).show()
+        val adapter = SleepNightAdapter(SleepNightListener {
+            nightId -> sleepTrackerViewModel.onSleepNightClicked(nightId)
         })
         binding.sleepList.adapter = adapter
 

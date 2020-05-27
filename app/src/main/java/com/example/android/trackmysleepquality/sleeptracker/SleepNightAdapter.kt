4756c55221056db4efa186e56cd542aaf20d8941
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-27 10:25:55 +0530

    step 11: adding a click listener, completed

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -25,14 +25,14 @@ import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 // TODO (04) Add a SleepNightListener reference to the SleepNightAdapter class declaration.
-class SleepNightAdapter : ListAdapter<SleepNight,
+class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<SleepNight,
         SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
 
         // TODO (05) Add clickListener parameter to holder.bind().
-        holder.bind(item)
+        holder.bind(getItem(position)!!, clickListener)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -44,8 +44,9 @@ class SleepNightAdapter : ListAdapter<SleepNight,
 
         // TODO (06) Add a clickListener parameter to the bind() function,
         // and add a binding for the clickListener.
-        fun bind(item: SleepNight) {
+        fun bind(item: SleepNight, clickListener: SleepNightListener) {
             binding.sleep = item
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
 
@@ -77,3 +78,6 @@ class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
 }
 
 // TODO (01) Create a new class called SleepNightListener.
+class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
+    fun onClick(night: SleepNight) = clickListener(night.nightId)
+}
